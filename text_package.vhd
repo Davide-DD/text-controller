ba76baf450a1cf7bb4a8d95abbe8de6d17e39b08
Davide Di Donato <davide364@gmail.com>
2018-04-21 19:53:52 +0200

    Initial commit

--- text_package.vhd
+++ text_package.vhd
@@ -3,20 +3,15 @@ use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
 package text_package is
+	
+	-- put all your strings here
+	constant TEXT_1   : string := "";
+	constant TEXT_2   : string := "";
 
-	constant FONT_WIDTH : integer := 8;
-	constant FONT_HEIGHT : integer := 16;
+	-- here is the message type definition: it has to contain all the labels of the strings you defined before 
+	type message is (TEXT_1, TEXT_2);
 	
-	constant TEXT_X : integer := 512/2;
-	constant TEXT_Y : integer := 480/2;
-	
-	constant TITLE_TEXT   : string := "M A S T E R M I N D";
-	constant INTRO_TEXT   : string := "Premi un qualsiasi KEY da 1 a 3 per iniziare a giocare";
-	constant WIN_TEXT     : string := "Hai vinto! La combinazione e' corretta!";
-	constant LOSE_TEXT    : string := "Hai perso! La combinazione era:";
-	
-	type message is (TITLE, INTRO, WIN, LOSE);
-	
+	-- here is the codes type definition: don't worry about it, you can leave this as it is
 	type codes is array(natural range<>) of integer;
 
 end package;
